0e905619d5f22860a6bc546e434afe62f3f79f70
esmaigua <maiguae12@gmail.com>
2025-05-13 18:35:31 -0500

    Actualizar app.py para usar PORT y host 0.0.0.0

--- app.py
+++ app.py
@@ -8,5 +8,7 @@ def hello():
     return "¡Hola Mundo desde helloWorldPy en Railway!"
 
 if __name__ == "__main__":
+    import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
